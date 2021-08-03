856a12b2021cd11dd60811008e674f8b1fc244ad
Roman Maksimov <get.my.email.name@yandex.ru>
2021-08-03 19:37:50 +0300

    js2 + css_practice3

---
+++ js/2/index.js
@@ -0,0 +1,31 @@
+// const, let, var
+// 
+// Типы данных
+// string: '' "" `` + экранирование
+// number: 5, 5.0, NaN, +-0, Infinity,
+// boolean: true/false
+// null
+// undefined
+// Symbol
+// BigInt
+//
+// Оператор typeof (typeof от typeof)
+// 
+// $_
+let helloWolrd = true;
+
+const type = "number";
+
+
+console.log(helloWolrd);
+console.log(typeof "number"); // выводим тип данных
+
+// heuqghqigheq
+// hfequhfqui
+
+/*  
+
+ feqjfqef
+ feqhufhqefq
+ feqhufqiufq
+*/
