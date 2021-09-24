6df98b3a7fa14040e46e1263a4e7b4d1afd84c52
R1ON <r.maximov2012@yandex.ru>
2021-09-24 12:22:36 +0300

    js 5

---
+++ js/5/index.js
@@ -0,0 +1,104 @@
+// Операторы
+
+const a = 10;
+const b = 5;
+let c = 1;
+
+// 1. Арифметические (+ - / * % ++ -- **)
+
+// ++ Инкремент (плюс 1)
+// -- Декремент (минус 1)
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a ** b);
+// console.log(a % b);
+// console.log(c);
+
+// c--;
+
+// console.log(c);
+
+
+
+// 2. Приоритет арифметики ()
+
+// console.log((2 + 2) * 2)
+
+
+// 3. + для строк и чисел
+
+// console.log(5 + 5);
+// console.log('Hello' + ' ' + 'world');
+
+
+// 4. Присваивания (=   +=   -=   /=   *=)
+
+// let z = 0;
+
+// feqfjeqof..
+
+// z = z + 1;
+// z += 1;
+
+// z = z - 1;
+// z -= 1;
+
+
+// 5. Сравнения (== !=   === !==   > >=   < <=)
+
+// // console.log('hello' != 'hello');
+// // console.log('hello' !== 'hello');
+// // console.log('5' === 5);
+
+// console.log(10 >= 11);
+// console.log(10 >= 10);
+// console.log(10 >= 9);
+
+// console.log(10 <= 11);
+// console.log(10 <= 10);
+// console.log(10 <= 9);
+
+
+// === значению + типу
+// == значению (лучше не использовать)
+
+
+// 6. Унарные (+ -)
+
+// console.log(typeof +'5')
+// console.log(+'5' + 5)
+
+// const z = -5;
+// console.log(-z);
+
+// 7. Логические (&& || !)
+// На следующую тему
+
+
+// 8. in
+// const obj = {
+// 	name: 'Alex',
+// 	age: 51,
+// };
+
+// console.log('email' in obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
