7c5796ce41cfbd8a4313e8661ad5c502ec082425
R1ON <r.maximov2012@yandex.ru>
2022-02-13 21:21:39 +0300

    js17.2+js_practise4

---
+++ js_practice/4/index.js
@@ -0,0 +1,57 @@
+const news = [
+	{
+		title: 'Я сегодня поел',
+		subtitle: 'Курицу',
+		content: 'Сегодня был веселый день...',
+		images: ['https://fueqfuqehfeq', 'https://jfiq'],
+		links: ['https://koklp', 'https://12312'],
+		partners: [{
+			title: 'Мы за все хорошее, ',
+			site: 'https://nfjefq',
+			keywords: ['Мы хорошие', 'Мы красивые'],
+		}, {
+			title: 'Мы за все хорошее2, ',
+			site: 'https://nfjefq2',
+			keywords: ['Мы хорошие2', 'Мы красивые2'],
+		}],
+	},
+	{
+		title: 'Я сегодня поел3',
+		subtitle: 'Курицу3',
+		content: 'Сегодня был веселый день...3',
+		images: ['https://fueqfuqehfeq', 'https://jfiq'],
+		links: ['https://koklp', 'https://12312'],
+		partners: [{
+			title: 'Мы за все хорошее, ',
+			site: 'https://nfjefq',
+			keywords: ['Мы хорошие', 'Мы красивые'],
+		}, {
+			title: 'Мы за все хорошее2, ',
+			site: 'https://nfjefq2',
+			keywords: ['Мы хорошие2', 'Мы красивые2'],
+		}],
+	},
+];
+
+// const keywords = news[0].partners[1].keywords;
+// const image = news[0].images[1];
+
+const title = news[1].title;
+const partnerTitle = news[1].partners[0].title;
+
+console.log(title, partnerTitle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const title = news[0].title;
+// const subtitle = news[0].subtitle;
