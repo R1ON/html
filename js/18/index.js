d070ea7b848ad732de451fbb1d7f01c7a78182b9
R1ON <r.maximov2012@yandex.ru>
2022-05-23 20:49:36 +0300

    js_18 + css_grid_11

---
+++ js/18/index.js
@@ -0,0 +1,43 @@
+// Number
+// Math.pow / **
+// Math.ceil - к большему
+// Math.floor - к меньшему
+// Math.round - до ближайшего целого
+// Math.random
+// parseInt, parseFloat, toFixed
+// унарный +, Number, toString
+// isNaN, isInteger
+
+
+// console.log(Math.ceil(0.1));
+// console.log(Math.ceil(0.5));
+// console.log(Math.ceil(0.9));
+
+// console.log(Math.floor(0.1));
+// console.log(Math.floor(0.5));
+// console.log(Math.floor(0.9));
+
+// console.log(Math.round(0.1)); // 0
+// console.log(Math.round(0.4)); // 0
+// console.log(Math.round(0.5)); // 1
+// console.log(Math.round(0.9)); // 1
+
+// const test = 5.999999999999999999999;
+
+// console.log(test.toFixed(3));
+// console.log(test.toFixed(0));
+
+// console.log(Number.parseFloat('9.99px'))
+
+// const test = 15;
+// console.log(typeof test.toString());
+
+// console.log(2 ** 5);
+// console.log(Math.pow(2, 5));
+
+// const test = 123.12;
+
+// console.log(Number.isInteger(test));
+
+console.log(Math.floor(Math.random() * 10 + 1));
+
